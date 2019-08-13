7352381e50f88c1dd30bbdd6b444af2ed3d66497
Supernatural802 <gabrieal@gmail.com>
2019-08-13 12:52:31 -0400

    Add files via upload

--- HNW/300x600/scripts.js
+++ HNW/300x600/scripts.js
@@ -36,40 +36,40 @@
 
 
 
-    t.to(blueWipe, 1, {x:"-=205", ease: Power3.easeOut, delay:2});
+    t.to(blueWipe, 1, {x:"-=205", ease: Power3.easeOut, delay:3});
 
 
-    t.to(copy01, 1, { autoAlpha:1, ease: Power3.easeOut, delay:2.5});
-    t.from(copy01, 1, { y:"+=20",  rotation: .01, willChange: "transform", force3D:true, ease: Power3.easeOut, delay:2.5});
+    t.to(copy01, 1, { autoAlpha:1, ease: Power3.easeOut, delay:3.5});
+    t.from(copy01, 1, { y:"+=20",  rotation: .01, willChange: "transform", force3D:true, ease: Power3.easeOut, delay:3.5});
 
-    t.to(copy02, 1, { autoAlpha:1, ease: Power3.easeOut, delay:2.8});
-    t.from(copy02, 1, { y:"+=20",  rotation: .01, willChange: "transform", force3D:true, ease: Power3.easeOut, delay:2.8});
+    t.to(copy02, 1, { autoAlpha:1, ease: Power3.easeOut, delay:3.8});
+    t.from(copy02, 1, { y:"+=20",  rotation: .01, willChange: "transform", force3D:true, ease: Power3.easeOut, delay:3.8});
 
-    t.to(copy03, 1, { autoAlpha:1, ease: Power3.easeOut, delay:3.1});
-    t.from(copy03, 1, { y:"+=20",  rotation: .01, willChange: "transform", force3D:true, ease: Power3.easeOut, delay:3.1});
+    t.to(copy03, 1, { autoAlpha:1, ease: Power3.easeOut, delay:4.1});
+    t.from(copy03, 1, { y:"+=20",  rotation: .01, willChange: "transform", force3D:true, ease: Power3.easeOut, delay:4.1});
 
-    t.to(copy04, 1, { autoAlpha:1, ease: Power3.easeOut, delay:3.4});
-    t.from(copy04, 1, { y:"+=20",  rotation: .01, willChange: "transform", force3D:true, ease: Power3.easeOut, delay:3.4});
+    t.to(copy04, 1, { autoAlpha:1, ease: Power3.easeOut, delay:4.4});
+    t.from(copy04, 1, { y:"+=20",  rotation: .01, willChange: "transform", force3D:true, ease: Power3.easeOut, delay:4.4});
 
-    t.to(copy05, 1, { autoAlpha:1, ease: Power3.easeOut, delay:3.7});
-    t.from(copy05, 1, { y:"+=20",  rotation: .01, willChange: "transform", force3D:true, ease: Power3.easeOut, delay:3.7});
+    t.to(copy05, 1, { autoAlpha:1, ease: Power3.easeOut, delay:4.7});
+    t.from(copy05, 1, { y:"+=20",  rotation: .01, willChange: "transform", force3D:true, ease: Power3.easeOut, delay:4.7});
 
-    t.to(copy06, 1, { autoAlpha:1, ease: Power3.easeOut, delay:4.5});
+    t.to(copy06, 1, { autoAlpha:1, ease: Power3.easeOut, delay:5.5});
     //t.from(copy04, 1, { y:"+=15",  rotation: .01, willChange: "transform", force3D:true, ease: Power3.easeOut, delay:6.5});
 
 
 
-    t.to(blueWipe, .5, { x:205, ease: Power2.easeInOut, delay:7.5});
-    t.to(container, .5, { x:250, ease: Power2.easeInOut, delay:7.5});
+    t.to(blueWipe, .5, { x:205, ease: Power2.easeInOut, delay:8.5});
+    t.to(container, .5, { x:250, ease: Power2.easeInOut, delay:8.5});
 
-    t.from(ladderHide, 0, {autoAlpha:0, ease: Power2.easeInOut, delay:7.5})
-    t.to(img01, .5, { autoAlpha:0, ease: Power2.easeInOut, delay:7.3});
-    t.from(logo, .5, {autoAlpha:0, ease: Power2.easeOut, delay:8});
-    t.from(legal, .5, {autoAlpha:0, ease: Power1.easeOut, delay:9});
+    t.from(ladderHide, 0, {autoAlpha:0, ease: Power2.easeInOut, delay:8.5})
+    t.to(img01, .5, { autoAlpha:0, ease: Power2.easeInOut, delay:8.3});
+    t.from(logo, .5, {autoAlpha:0, ease: Power2.easeOut, delay:9});
+    t.from(legal, .5, {autoAlpha:0, ease: Power1.easeOut, delay:10});
 
-    t.from(cta, .5, {autoAlpha:0, willChange: "transform", force3D:true, ease: Power2.easeOut, delay:8.5,  onComplete: function() { rolloverActive = true } });
-    t.to(cta, .5, {scale:1.15, rotation: .01, willChange: "transform", force3D:true, ease: Power3.easeOut, delay:9.5});
-    t.to(cta, .5, {scale:1, rotation: .01, willChange: "transform", force3D:true, ease: Power3.easeOut, delay:9.7});
+    t.from(cta, .5, {autoAlpha:0, willChange: "transform", force3D:true, ease: Power2.easeOut, delay:9.5,  onComplete: function() { rolloverActive = true } });
+    t.to(cta, .5, {scale:1.15, rotation: .01, willChange: "transform", force3D:true, ease: Power3.easeOut, delay:10.5});
+    t.to(cta, .5, {scale:1, rotation: .01, willChange: "transform", force3D:true, ease: Power3.easeOut, delay:10.7});
 
 
 
